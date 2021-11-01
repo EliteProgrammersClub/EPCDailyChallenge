cdb7fc5ab301a3bb37fd6a76ff81f17b10b5d6c4
andreaPorrazzo <andreaporrazzo23@gmail.com>
2021-11-01 11:00:14 +0100

    add reverse funcion in javascript

---
+++ November/Day 1/Javascript/javascript.js
@@ -0,0 +1,14 @@
+function reverse(s){
+    return s.split("").reverse().join("");
+}
+
+var string1 = reverse("adam");
+var string2= reverse("John!");
+var apple= reverse("apple");
+var hello= reverse("hello");
+var greetings= reverse("Greetings!");
+console.log(string1);
+console.log(string2);
+console.log(apple);
+console.log(hello);
+console.log(greetings);
