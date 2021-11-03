9ee6b18c3abc60ab7eaba75e8812fa36d3407631
mtrishal123 <60638143+mtrishal123@users.noreply.github.com>
2021-11-03 10:49:45 +0530

    Update reverseString.java
    Added Comments to make the code more readable

--- November/Day 2/Java/reverseString.java
+++ November/Day 2/Java/reverseString.java
@@ -4,15 +4,15 @@ public class ReverseString {
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a String ");
-        String n=sc.next();
-        String rev=reverse(n);
+        String n=sc.next();// we are taking a user input here
+        String rev=reverse(n);// We are passing the input to function which will reverse any string
         System.out.println(rev);
         sc.close();
     }
     public static String reverse(String str) {
-        String w="";
+        String w="";// declaring and initialising a temp variable with blank,to store the value and return the string
         for(int i=0;i<str.length();i++) {
-            w=str.charAt(i)+w;
+            w=str.charAt(i)+w; // Since it's a String,we can add any character at any part of the String...Here I am adding the character extracted,at the front of w to get the final reverse value 
         }
         return w;
     }
