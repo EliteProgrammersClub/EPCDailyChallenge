fbacd6705e58d7cf0b7f9f430dee798bd4fe64a2
xxxSkypper <kevinkila2007@gmail.com>
2021-11-08 23:51:00 +0100

    Palindrome

---
+++ November/Palindrome.js
@@ -0,0 +1,18 @@
+// [November]/[Day 4]/[Javascript]/{Palidrome.js}
+
+/* Given a string, return true if the string is a palindrome or false if it is not. Palindromes are strings that form the same word if it is reversed. Do include spaces and punctuation in determining if the string is a palindrome. Make use of Functions and Methods where necessory */
+
+function palidrome(str){
+    var len = str.length;  // assign the length of string
+    var mid = Math.floor(len / 2);  //round down the length of string
+
+// checking the string
+    for (let i = 0; i < mid; i++){
+        if (str[i] !== str[len - 1 - i]) {  // check whether the string is thesame when it is reverse
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(palidrome("abba"));  // true
