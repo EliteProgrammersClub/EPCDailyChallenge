c764b703ecbf68e8e98612c776d98015d8236877
xaster-Kies <samueledward40@gmail.com>
2021-11-09 18:47:23 +0100

    comment max char

--- November/Day 4/Javascript/Max Char.js
+++ November/Day 4/Javascript/Max Char.js
@@ -3,16 +3,16 @@
 // Description: A Program to get the highest number of chars from a string
 
 function maxChar(str) {
-    let string = "The max Character"
-    let chars = {};
+    let string = "The max Character" //Getting the max character
+    let chars = {}; // Initializing an empty chars array
 
-    for(let char of string) {
-        if(!chars[char]) {
-            char = 1;
+    for(let char of string) { // Looping through the string
+        if(!chars[char]) { // Checking if the position in the string is not present in the array
+            char = 1; // if it is true it will initialize char to 1
         } else {
-            char++
+            char++; // if it is present it will increment the value of char
         }
     }
 
-    return chars;
+    return chars; // Returning the array of characters
 }
---
+++ November/Day 9/Javascript/fizzbuzz.js
@@ -0,0 +1,8 @@
+// By @Samuel Tiokeng
+// on Nov 9 2021
+// Description: Program to find the fizzbuzz where highest
+
+
+function fizzbuzz(n) {
+    
+}
