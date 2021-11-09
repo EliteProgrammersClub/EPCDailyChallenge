d22de58216f4ff0402b439ded2cad3349c1d13c6
xaster-Kies <samueledward40@gmail.com>
2021-11-09 18:56:04 +0100

    fizz buzz javascript

--- November/Day 9/Javascript/fizzbuzz.js
+++ November/Day 9/Javascript/fizzbuzz.js
@@ -1,8 +1,22 @@
 // By @Samuel Tiokeng
 // on Nov 9 2021
-// Description: Program to find the fizzbuzz where highest
+// Description: Program to find the fizzbuzz. Where if the entered value is a multiple of 3 and 5, it prints fizz and buzz
+// respectively and when both it prints fizzbuzz 
 
 
 function fizzbuzz(n) {
-    
+    for(let i = 1; i < n; i++) {
+        if(n % 3) {
+            console.log("fizz")
+        } 
+        else if(n % 5) {
+            console.log("buzz")
+        }
+        else if((n % 3) && (n % 5)) {
+            console.log("fizzbuzz")
+        }
+        else {
+            console.log(i)
+        }
+    }
 }
